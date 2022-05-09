816d8575941f83af200a9d6e74ec46dc089f3038
eished <kished@outlook.com>
2022-05-09 15:09:26 +0800

    第五章完结

---
+++ 全面系统Python3.8入门+进阶.assets/code/5.12.py
@@ -0,0 +1,12 @@
+a = 1
+print(type(a) == int)
+
+b = 'hi'
+print(type(b) == str)
+
+test1 = isinstance(a, str)
+print(test1)
+test1 = isinstance(a, int)
+print(test1)
+test1 = isinstance(b, str)
+print(test1)
---
+++ 全面系统Python3.8入门+进阶.assets/code/5.9.py
@@ -0,0 +1,4 @@
+print(True and True)
+print(False or 0)
+print(not 0)
+print(not "0")
